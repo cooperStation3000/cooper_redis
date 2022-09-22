a6a5e566b63768c0cbc9dc7fca5c7b10910226fe
wyswill <1151373571@live.com>
2022-09-23 00:29:09 +0800

    feat: add ui

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,31 @@
 <template>
-  <div class='mainpage'>
-  <n-button>asdfasd</n-button>
-  </div>
+  <n-layout has-sider>
+    <n-layout-sider  bordered content-style="padding: 24px; height:100vh" collapse-mode="transform">
+      <n-button type="info" size="large">
+        <template #icon>
+        <n-icon>
+          <cash-icon />
+        </n-icon>
+      </template>
+        创建链接</n-button>
+    </n-layout-sider>
+    <n-layout>
+      <n-layout-content content-style="padding: 24px;">
+        平山道
+      </n-layout-content>
+    </n-layout>
+  </n-layout>
 </template>
 
 <script lang="ts" setup>
-import {NButton} from'naive-ui'
-
+import { NButton, NLayout, NLayoutSider,  NLayoutContent,NIcon } from 'naive-ui'
+import { CashOutline as CashIcon } from '@vicons/ionicons5'
 
 </script>
+
+<style lang="less">
+
+
+
+
+</style>
