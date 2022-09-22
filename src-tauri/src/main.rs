891133b3c3c1b601717cfee333d83214d7db92b9
wyswill <www.1151373571@live.com>
2022-09-22 18:26:14 +0800

    feat: 添加redis 链接测试

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,7 +1,10 @@
 #![cfg_attr(
-    all(not(debug_assertions), target_os = "windows"),
-    windows_subsystem = "windows"
+all(not(debug_assertions), target_os = "windows"),
+windows_subsystem = "windows"
 )]
+extern crate redis;
+
+use redis::Commands;
 
 // Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
 #[tauri::command]
@@ -10,6 +13,9 @@ fn greet(name: &str) -> String {
 }
 
 fn main() {
+    let client = redis::Client::open("redis://127.0.0.1:6379/0").unwrap();
+    let mut con = client.get_connection().unwrap();
+    let _ : () = con.set("test","123123").unwrap();
     tauri::Builder::default()
         .invoke_handler(tauri::generate_handler![greet])
         .run(tauri::generate_context!())
