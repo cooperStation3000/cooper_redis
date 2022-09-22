0e8d5c0b76b525e1e9720c9e6cfcbdea38240728
wyswill <1151373571@live.com>
2022-09-22 23:23:53 +0800

    feat: fix redix error

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -3,22 +3,22 @@ all(not(debug_assertions), target_os = "windows"),
 windows_subsystem = "windows"
 )]
 
+use redis::Commands;
+
 mod redis_manager;
 
 extern crate redis;
 
-use redis::Commands;
-
 
 // Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
 #[tauri::command]
-fn greet(name: &str)  {
-    let mut con = redis_manager::RedisManager::get_redis_connection("redis://127.0.0.1:6379/0").expect("error");
+fn greet(name: &str)-> &str{
+    let mut con = redis_manager::get_redis_connection("redis://127.0.0.1:6379/0");
     let _: () = con.set("test", name).expect("set error");
+    name
 }
 
 fn main() {
-    let _: () = con.set("123", "123").expect("1231");
     tauri::Builder::default()
         .invoke_handler(tauri::generate_handler![greet])
         .run(tauri::generate_context!())
--- src-tauri/src/redis_manager.rs
+++ src-tauri/src/redis_manager.rs
@@ -2,12 +2,8 @@ extern crate redis;
 
 use redis::Connection;
 
-pub struct RedisManager {}
-
-impl RedisManager {
-    pub fn get_redis_connection(db_url: &str) -> Result<Connection, E> {
-        let mut  client = redis::Client::open(db_url).expect("redis 连接失败");
-        let mut con = client.get_connection().expect("redis 获取连接失败");
-        Ok(con)
-    }
+pub fn get_redis_connection(db_url: &str) -> Connection {
+    let client = redis::Client::open(db_url).expect("redis 连接失败");
+    let con = client.get_connection().expect("redis 获取连接失败");
+    con
 }
