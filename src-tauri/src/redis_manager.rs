d09e76d8613f7034fa0cd2252a7b235c6ffe4aea
wyswill <www.1151373571@live.com>
2022-10-09 17:04:23 +0800

    feat： 更新redis 写入方法

--- src-tauri/src/redis_manager.rs
+++ src-tauri/src/redis_manager.rs
@@ -4,9 +4,11 @@ pub struct Manager {
     pub client: Client,
 }
 
+static REDIS_URL: &str = "redis://127.0.0.1:6379";
+
 impl Manager {
     pub fn init() -> Self {
-        let client = redis::Client::open("redis://127.0.0.1:6379").expect("redis 连接失败");
+        let client = redis::Client::open(REDIS_URL).expect("redis 连接失败");
         Manager { client }
     }
 }
